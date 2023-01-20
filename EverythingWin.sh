b11b53efc725a9ae873367f6f9bdbe30d9033b69
ZonicTrout <117415204+ZonicTrout@users.noreply.github.com>
2023-01-20 09:11:47 -0800

    adding to bash script

--- EverythingWin.sh
+++ EverythingWin.sh
@@ -1,2 +1,3 @@
 #!/bash/bin
 curl https://launcher.mojang.com/download/Minecraft.exe -o MC
+curl https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-64-bit.exe -o GitBash.exe
